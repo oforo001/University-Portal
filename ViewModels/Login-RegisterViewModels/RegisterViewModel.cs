745b91910f10c269812da5f8f92509af5df83f61
Alex Forostianov <bezdelinik.1@gmail.com>
2025-11-02 16:48:51 +0100

    UN-14 Refactored admin creation feature. No hardcoded credentials more.

--- ViewModels/Login-RegisterViewModels/RegisterViewModel.cs
+++ ViewModels/Login-RegisterViewModels/RegisterViewModel.cs
@@ -4,20 +4,22 @@ namespace University_Portal.ViewModels
 {
     public class RegisterViewModel
     {
-        [Required(ErrorMessage = "Name is requered.")]
+        [Required(ErrorMessage = "Name is required.")]
         public string Name { get; set; }
-        [Required(ErrorMessage = "Email is requered.")]
+
+        [Required(ErrorMessage = "Email is required.")]
         [EmailAddress]
         public string Email { get; set; }
-        [Required(ErrorMessage = "Password is requered.")]
-        [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at {2} and max. {1} character")]
+
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at least {2} and max {1} characters.")]
         [DataType(DataType.Password)]
-        [Compare("ConfirmPassword", ErrorMessage = "Password does not match.")]
         public string Password { get; set; }
-        [Required(ErrorMessage = "Confirm password is requered.")]
+
+        [Required(ErrorMessage = "Confirm password is required.")]
         [DataType(DataType.Password)]
         [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
         public string ConfirmPassword { get; set; }
-
     }
 }
