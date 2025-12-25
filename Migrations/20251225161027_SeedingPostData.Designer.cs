f30d180f7ee111bf858fa7dc95079a873d9d5347
Alex Forostianov <bezdelinik.1@gmail.com>
2025-12-25 18:25:47 +0100

    Added: Blog models Added: PostController Added: PostViewModel Added: Create View

--- Migrations/20251225161027_SeedingPostData.Designer.cs
+++ Migrations/20251225161027_SeedingPostData.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using University_Portal.Data;
 
@@ -10,9 +11,11 @@ using University_Portal.Data;
 namespace University_Portal.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20251225161027_SeedingPostData")]
+    partial class SeedingPostData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
