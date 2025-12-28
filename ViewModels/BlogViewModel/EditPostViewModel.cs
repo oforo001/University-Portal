49c64c5978b72ff83631d800f54eca4b85e2dc9d
Alex Forostianov <bezdelinik.1@gmail.com>
2025-12-28 18:03:17 +0100

    Added: Full backend for blog

---
+++ ViewModels/BlogViewModel/EditPostViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using University_Portal.Models;
+
+namespace University_Portal.ViewModels.BlogViewModel
+{
+    public class EditPostViewModel
+    {
+        public Post Post { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> Categories { get; set; }
+        [ValidateNever]
+        public IFormFile? FeatureImage { get; set; }
+    }
+}
