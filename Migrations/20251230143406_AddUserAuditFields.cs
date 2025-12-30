0bcd61d2fafedc620fdb08d9e8b3ba870acb7abf
Alex Forostianov <bezdelinik.1@gmail.com>
2025-12-30 17:15:57 +0100

    Added: User management window

---
+++ Migrations/20251230143406_AddUserAuditFields.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace University_Portal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserAuditFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IsBlocked",
+                table: "AspNetUsers",
+                newName: "IsActive");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastLoginAt",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastUpdatedAt",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastLoginAt",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LastUpdatedAt",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "IsActive",
+                table: "AspNetUsers",
+                newName: "IsBlocked");
+        }
+    }
+}
--- Models/AppUser.cs
+++ Models/AppUser.cs
@@ -6,9 +6,17 @@ namespace University_Portal.Models
 {
     public class AppUser : IdentityUser
     {
+        public AppUser()
+        {
+            IsActive = true;
+            CreatedAt = DateTime.Now;
+        }
         [Required]
         [StringLength(100)]
         public string FullName { get; set; }
-        public bool IsBlocked { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? LastUpdatedAt { get; set; }
+        public DateTime? LastLoginAt { get; set; }
     }
 }
