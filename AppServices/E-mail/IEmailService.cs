6a4eb76053de9424d6d727f6181444036a7af987
Alex Forostianov <bezdelinik.1@gmail.com>
2026-02-21 15:43:53 +0100

    Added: SMTP Server Refactored: Registration & Login AJAX

---
+++ AppServices/E-mail/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using University_Portal.Models;
+
+namespace University_Portal.AppServices.E_mail
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(EmailDto request);
+    }
+}
