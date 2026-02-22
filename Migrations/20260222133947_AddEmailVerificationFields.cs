530ddf25bcfe65b36deca9a00a5a42e0204a3888
Alex Forostianov <bezdelinik.1@gmail.com>
2026-02-22 16:03:40 +0100

    Added: Full registration/verification logic and sending verification email to user's email Refactored: VerifyEmail.cshtml to use AJAX

---
+++ Migrations/20260222133947_AddEmailVerificationFields.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace University_Portal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailVerificationFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "EmailVerificationToken",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EmailVerificationTokenExpiry",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailVerificationToken",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "EmailVerificationTokenExpiry",
+                table: "AspNetUsers");
+        }
+    }
+}
