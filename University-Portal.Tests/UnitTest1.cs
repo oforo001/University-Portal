747665b6445559edf8166f6850085217fdafd98f
Oleksii Forostianov <143897315+oforo001@users.noreply.github.com>
2026-03-08 14:01:42 +0100

    Added: Test project (#32)

---
+++ University-Portal.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+using University_Portal.AppServices;
+
+namespace University_Portal.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true); 
+        }
+    }
+}
