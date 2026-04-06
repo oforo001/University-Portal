bead5e19645399986806d4f6353c5d0c1173048f
Alex Forostianov <bezdelinik.1@gmail.com>
2026-04-06 12:44:37 +0200

    Refactored: translated ENG -> PL

--- University-Portal/Models/Event.cs
+++ University-Portal/Models/Event.cs
@@ -5,35 +5,38 @@ namespace University_Portal.Models
 {
     public class Event
     {
-      //  Reprezentuje jedno wydarzenie organizowane na uczelni(np.wykład, spotkanie integracyjne, warsztaty)
-
+        // Reprezentuje jedno wydarzenie organizowane na uczelni (np. wykład, spotkanie integracyjne, warsztaty)
 
         [Key]
-        public int Id { get; set; } // DB column
+        public int Id { get; set; } // Kolumna w DB
+
         [Required]
-        [StringLength(200, MinimumLength = 2, ErrorMessage = "Provide Title with min. 2 and max. 200 characters")]
-        public string Title { get; set; } // DB column
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "Podaj tytuł o długości od 2 do 200 znaków")]
+        public string Title { get; set; } // Kolumna w DB
+
         [Required]
-        [StringLength(5000, MinimumLength = 2, ErrorMessage = "Provide Description with min. 2 and max. 5000 characters")]
-        public string Description { get; set; } // DB column
+        [StringLength(5000, MinimumLength = 2, ErrorMessage = "Podaj opis o długości od 2 do 5000 znaków")]
+        public string Description { get; set; } // Kolumna w DB
+
         public string? ImagePath { get; set; }
+
         [Required]
-        public DateTime Date { get; set; } // DB column
+        public DateTime Date { get; set; } // Kolumna w DB
+
         [Required]
-        [StringLength(1000, MinimumLength = 2, ErrorMessage = "Provide Location with min. 2 and max. 1000 characters")]
-        public string Location { get; set; } // DB column
+        [StringLength(1000, MinimumLength = 2, ErrorMessage = "Podaj lokalizację o długości od 2 do 1000 znaków")]
+        public string Location { get; set; } // Kolumna w DB
+
         [Required]
-        [Range(1, int.MaxValue, ErrorMessage = "Max participants must be greater than 0")]
-        public int MaxParticipants { get; set; } // DB column
+        [Range(1, int.MaxValue, ErrorMessage = "Maksymalna liczba uczestników musi być większa niż 0")]
+        public int MaxParticipants { get; set; } // Kolumna w DB
+
         [Required]
-        public string OrganizerId { get; set; } // DB FK column
+        public string OrganizerId { get; set; } // Kolumna FK w DB
+
         [ForeignKey("OrganizerId")]
-        public virtual AppUser Organizer { get; set; } 
+        public virtual AppUser Organizer { get; set; }
 
         public virtual ICollection<EventRegistration> Registrations { get; set; }
-
-
-
-
     }
 }
--- University-Portal/ViewModels/Login-RegisterViewModels/RegisterViewModel.cs
+++ University-Portal/ViewModels/Login-RegisterViewModels/RegisterViewModel.cs
@@ -4,22 +4,22 @@ namespace University_Portal.ViewModels
 {
     public class RegisterViewModel
     {
-        [Required(ErrorMessage = "Name is required.")]
+        [Required(ErrorMessage = "Imię i nazwisko jest wymagane.")]
         public string Name { get; set; }
 
-        [Required(ErrorMessage = "Email is required.")]
+        [Required(ErrorMessage = "Adres e-mail jest wymagany.")]
         [EmailAddress]
         public string Email { get; set; }
 
-        [Required(ErrorMessage = "Password is required.")]
-        [StringLength(40, MinimumLength = 8, ErrorMessage = "The {0} must be at least {2} and max {1} characters.")]
+        [Required(ErrorMessage = "Hasło jest wymagane.")]
+        [StringLength(40, MinimumLength = 8, ErrorMessage = "{0} musi mieć co najmniej {2} i maksymalnie {1} znaków.")]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
-        [Required(ErrorMessage = "Confirm password is required.")]
+        [Required(ErrorMessage = "Potwierdzenie hasła jest wymagane.")]
         [DataType(DataType.Password)]
-        [Display(Name = "Confirm Password")]
-        [Compare("Password", ErrorMessage = "Passwords do not match.")]
+        [Display(Name = "Potwierdź hasło")]
+        [Compare("Password", ErrorMessage = "Hasła nie są zgodne.")]
         public string ConfirmPassword { get; set; }
     }
 }
